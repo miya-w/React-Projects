8d874f25a90042c110d7fcb68ba1b031b00ec809
miya-w <62531644+hellomiyawu@users.noreply.github.com>
2022-03-22 22:03:34 +1100

    update

---
+++ 06-react-shopping-list/README.md
@@ -0,0 +1,38 @@
+# React todo-list Project
+![shopping-list](https://github.com/miya-w/React-Projects/blob/main/06-react-shopping-list/imgs/shoppinglist01.png)
+---
+**What will you learn in this React project?**
+CRUD -> Create, Read, Update, Delete
+### 1.reate & Read
+- complex state & Javascript ES6 Spread Operator
+` function addItems(){
+    setItems(prevItems => {
+      // Javascript ES6 Spread Operator
+      return [...prevItems, inputText];
+    })
+    // set the input text to an empty
+    setInputText(" ")
+  }`
+
+---
+### 2.map() method & arrow function
+`{items.map(todoItem =>{return<li>{todoItem}</li>})}`
+map() method
+arrow function
+[Arrow function expressions](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions)
+
+### 3.Delete function
+`
+ {items.map((todoItem, index)=> (            
+          < ToDoItem key = {index} id={index} text={todoItem} onChecked={deleteItem()}/>)
+          )}
+`
+
+---
+## Getting Started with Create React App
+
+npm start -  Starts the development server.
+
+npm run - build Bundles the app into static files for production.
+
+npm test - Starts the test runner.
---
+++ 06-react-shopping-list/src/components/App.jsx
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
+
+  function deleteItem(id) {
+    setItems(prevItems => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+// import React, {useState} from  "react";
+// import ToDoItem from "./ToDoItem";
+
+// function App() {
+
+//   const [inputText, setInputText] = useState(" ");
+//   const [items, setItems] = useState([]);
+    
+//   function handleChange(event){
+//       const newValue = event.target.value; 
+//       setInputText(newValue);
+//     }
+  
+//     function addItems(){
+//       setItems(prevItems => {
+//         // Javascript ES6 Spread Operator
+//         return [...prevItems, inputText];
+//       })
+//       // set the input text to an empty
+//       setInputText(" ")
+//     }
+
+//     function deleteItem(id){
+//       setItems((prevItems)=>{
+//         // filter array
+//           return prevItems.filter(
+//             (item, index)=>{
+//               return index !== id;
+//             }
+//           );
+
+//       });
+//     }
+  
+//   return (
+//     <div className="container">
+//       <div className="heading">
+//         <h1>My Shopping List</h1>
+       
+//       </div>
+//       <div className="form">
+//         <input 
+//           type="text"
+//           value={inputText}
+//           onChange={handleChange}
+//            />
+//         <button onClick={addItems}>
+//           <span>Add</span>
+//         </button>
+//       </div>
+//       <div>
+//         <ul>
+//           <li>A Item </li>
+//           {/* arrow function */}
+//           {/* {items.map(todoItem =>{return<li>{todoItem}</li>})} */}
+//           {items.map((todoItem, index)=> (            
+//           < ToDoItem key = {index} id={index} text={todoItem} onChecked={deleteItem()}/>)
+//           )}
+//           <div>         
+//           </div>                 
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+// export default App;
